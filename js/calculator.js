dfe6efef912551e3c6dde704cf1dc1d666ade9b0
Kevin Guo <Kevin@Kevins-Air.home>
2019-06-21 21:46:16 -1000

    Start making buttons do stuff

---
+++ js/calculator.js
@@ -0,0 +1,81 @@
+/*make '+-/*' and = operators buttons
+    make clear, balance, deposit, widthraw button
+    [________________] <-----( display )
+
+    [7] [8]  [9]   [÷]  [clear]
+    [4] [5]  [6]   [×]  [get balance]
+    [1] [2]  [3]   [-]  [deposit cash]
+    [0] [00] [.]   [+]  [withdraw cash]
+                   [=]
+    */
+
+var register = (function() {
+    let balance = 0; //what you see on the calculator
+    let total = 0;
+    let memory = 0; //what the balance totals to
+    let clearBalance = 0;
+    let registerTotal = 0;
+    let calculator = {};
+    
+    calculator.getBalance = function() {
+        balance = memory;
+        return balance;
+    }
+
+    calculator.getRegisterTotal = function() {
+        console.log("You have " + registerTotal + " left in your register")
+        return registerTotal;
+    }
+
+    calculator.clearBalance = function() {
+        console.log("I cleared my balance of " + balance)
+        balance = clearBalance;
+        return total;
+    }
+
+    calculator.depositCash = function(x) {
+        registerTotal = registerTotal + x
+        return registerTotal;
+    }
+
+    calculator.withdrawCash = function (x) {
+        registerTotal = registerTotal - x
+        return registerTotal;
+    }
+
+    calculator.add = function(x) {
+        console.log("added " + x)
+        memory += x;
+    }
+
+    calculator.subtract = function(x) {
+        console.log("subtracted " + x)
+        memory -= x;
+    }
+
+    calculator.multiply = function(x) {
+        console.log("multiplied " + x)
+        memory *= x;
+    }
+
+    calculator.divide = function(x) {
+        console.log("divided " + x)
+        memory /= x;
+    }
+
+    calculator.orderFood = function() {
+        console.log("i bought " + balance + " of items")
+        registerTotal -= balance
+    }
+
+    return calculator;
+
+})();
+
+register.depositCash(250)
+console.log(register.add(50))
+console.log(register.add(50))
+console.log(register.getBalance())
+// register.clearBalance()
+register.orderFood()
+console.log(register.getRegisterTotal())
---
+++ js/cash_register.js
@@ -0,0 +1,117 @@
+// function buttonMaker(str1, str2, str3, str4) {
+//     str1 = document.createElement(str2);
+//     str1.className = str3;
+//     str1.innerHTML = str4
+// }
+
+// for (let i = 0; i <= 9; i++) {
+//     buttonMaker("btn" + i,"button","btn",i)
+// }
+
+//Create all buttons
+
+let btn1 = document.createElement('button')
+btn1.className = "btn"
+btn1.innerHTML = "1";
+let btn2 = document.createElement('button')
+btn2.innerHTML = "2";
+btn2.className = "btn"
+let btn3 = document.createElement('button')
+btn3.innerHTML = "3";
+btn3.className = "btn"
+let btn4 = document.createElement('button')
+btn4.innerHTML = "4";
+btn4.className = "btn"
+let btn5 = document.createElement('button')
+btn5.innerHTML = "5";
+btn5.className = "btn"
+let btn6 = document.createElement('button')
+btn6.innerHTML = "6";
+btn6.className = "btn"
+let btn7 = document.createElement('button')
+btn7.innerHTML = "7";
+btn7.className = "btn"
+let btn8 = document.createElement('button')
+btn8.innerHTML = "8";
+btn8.className = "btn"
+let btn9 = document.createElement('button')
+btn9.innerHTML = "9";
+btn9.className = "btn"
+let btn0 = document.createElement('button')
+btn0.innerHTML = "0";
+btn0.className = "btn"
+let btn00 = document.createElement('button')
+btn00.innerHTML = "00";
+btn00.className = "btn"
+let btnDecimal = document.createElement('button')
+btnDecimal.innerHTML = "."
+btnDecimal.className = 'btn'
+let btnAdd = document.createElement('button')
+btnAdd.innerHTML = "+"
+btnAdd.className = "btn"
+let btnSubtract = document.createElement('button')
+btnSubtract.innerHTML = "-"
+btnSubtract.className = "btn"
+let btnMultiply = document.createElement('button')
+btnMultiply.innerHTML = "*"
+btnMultiply.className = "btn"
+let btnDivide = document.createElement('button')
+btnDivide.innerHTML = "/"
+btnDivide.className = "btn"
+let btnEquals = document.createElement('button')
+btnEquals.innerHTML = "="
+btnEquals.className = "btn"
+let btnBalance = document.createElement('button')
+btnBalance.innerHTML = "Balance"
+btnBalance.className = "btn"
+let btnDeposit = document.createElement('button')
+btnDeposit.innerHTML = "Deposit"
+btnDeposit.className = 'btn'
+let btnWithdraw = document.createElement('button')
+btnWithdraw.innerHTML = "Withdraw"
+btnWithdraw.className = 'btn'
+let btnClear = document.createElement('button')
+btnClear.innerHTML = "Clear"
+btnClear.className = 'btn'
+let btnPay = document.createElement('button')
+btnPay.innerHTML = "Pay"
+btnPay.className = 'btn'
+
+let findRows = document.getElementsByClassName("calcBtn")
+//Row 1
+
+findRows[0].appendChild(btn7)
+findRows[0].appendChild(btn8)
+findRows[0].appendChild(btn9)
+findRows[0].appendChild(btnDivide)
+findRows[0].appendChild(btnClear)
+
+//Row 2
+
+findRows[1].appendChild(btn4)
+findRows[1].appendChild(btn5)
+findRows[1].appendChild(btn6)
+findRows[1].appendChild(btnMultiply)
+findRows[1].appendChild(btnBalance)
+
+//Row 3
+
+findRows[2].appendChild(btn1)
+findRows[2].appendChild(btn2)
+findRows[2].appendChild(btn3)
+findRows[2].appendChild(btnSubtract)
+findRows[2].appendChild(btnDeposit)
+
+//Row 4
+
+findRows[3].appendChild(btn0)
+findRows[3].appendChild(btn00)
+findRows[3].appendChild(btnDecimal)
+findRows[3].appendChild(btnAdd)
+findRows[3].appendChild(btnWithdraw)
+
+//Row 5
+findRows[4].appendChild(btnPay)
+
+let findButtons = document.querySelectorAll(".btn")
+console.log(findButtons)
